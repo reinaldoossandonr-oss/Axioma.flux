e638a01d635d558454292630741cd839d275220f
reinaldoossandonr-oss <reinaldoossandonr@gmail.com>
2026-06-29 18:56:26 +0200

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
+
 // Asegúrate de que esta sea la forma correcta de inicializar en tu versión de la librería
 const SUPABASE_URL = 'https://legtxgdwqjfzvlvheaao.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Mantenla igual
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlZ3R4Z2R3cWpmenZsdmhlYWFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwNjM5MDAsImV4cCI6MjA5NzYzOTkwMH0.EXACa14BiJshtfU8i-1SmpjTtOYjlCjyNUiazd8RX20'; // Usa la misma llave que ya confirmamos
+
 
 const clienteAuth = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
